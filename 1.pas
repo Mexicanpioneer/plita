2e69f45d1699fb89f8bec4b31d1cf85d7973c516
Mexicanpioneer <127957583+Mexicanpioneer@users.noreply.github.com>
2023-04-02 22:38:31 +0800

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,21 @@
+﻿Program plita;
+Var
+  a, b, c, v, p, pp: real;
+ const
+   h : integer = 3500;
+   h1 : integer = 4000;
+Begin
+ writeln ('Данное приложение считает стоимость фундаментa');
+ write ('Введите длину a=');
+ readln (a);
+ write ('Введите ширину b=');
+ readln (b);
+ write ('Введите высоту c=');
+ readln (c);
+ v := a*b*c;
+ p := v*h;
+ pp := v*h1;
+ writeln ('Объем бетона v=', v);
+ writeln ('Стоимость бетона марки М250 ', p);
+ writeln ('Стоимость бетона марки М300 ', pp)
+End.
